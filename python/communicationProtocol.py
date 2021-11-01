e2bd2ac7b3f7fba8f3568c82b36f8d38bd64c453
Kirk Boyd <kirkland.boyd@trincoll.edu>
2021-11-01 16:57:54 -0600

    add tentative comments to communicationProtocol.py

--- python/communicationProtocol.py
+++ python/communicationProtocol.py
@@ -10,8 +10,8 @@ posRobx = 700
 posRoby = 150
 posRobt = 0
 
-ser = serial.Serial('COM3',9600) #Windows
-#ser = serial.Serial('\dev\ttyUSB*',9600) #Unix
+ser = serial.Serial('COM3',9600) #Windows serial port
+#ser = serial.Serial('\dev\ttyUSB*',9600) #Unix serial port
 
 #List of Commands or functions
     #Input
@@ -36,7 +36,7 @@ ser = serial.Serial('COM3',9600) #Windows
             #inA2
 
 
-def pull():
+def pull(): #pulls (or receives) data from the arduino on the pi
     global posRobx
     global posRoby
     global posRobt
@@ -46,10 +46,10 @@ def pull():
     global posBally
 
 
-    START_MARKER = '<'
-    END_MARKER = '>'
-    COMMAND_SEP = '|'
-    VALUE_SEP = '-'
+    START_MARKER = '<'  #marks the beginning of a data packet
+    END_MARKER = '>'    #marks the end of a data packet
+    COMMAND_SEP = '|'   #marks a separation between commands within a packet
+    VALUE_SEP = '-'     #??
 
     receiving = False   #set to true when start marker is received, set to false when end marker is received
     commandReceived = False    #set to true when command separator is received (or if command buffer is full)
@@ -61,24 +61,24 @@ def pull():
     cmdIndex = 0
     if (ser.in_waiting > 0):
         packet = ser.readline().decode("utf-8").replace("\n", "") #Read in line, convert to string, remove new line character
-        print(packet)
+        print(packet) #print what was received from the serial port
         if (isWhiteSpace(packet)):
             return # Ignore whitespace
-        while index < len(packet):
-            serialByte = packet[index]
+        while index < len(packet): #step through each byte of the packet
+            serialByte = packet[index] #the byte we are looking at is the one currently at index
             # print(serialByte)
-            if (isWhiteSpace(serialByte)):
+            if (isWhiteSpace(serialByte)): #ignore whitespace again if found
                 return
-            if serialByte == START_MARKER:
-                receiving = True
-                commandReceived = True
+            if serialByte == START_MARKER: #if start marker is found
+                receiving = True #record that a packet is beginning to be received
+                commandReceived = True #record that a packet was in fact received
                 index = index + 1
                 continue
-            if(receiving):
+            if(receiving): #if looking for a packet
                 serialByte = packet[index]
                 if not commandReceived:
                     if (serialByte == COMMAND_SEP): #If the command separator is received
-                        index = index + 1
+                        index = index + 1 #count forward one because there is not a command in this byte
                         commandReceived = True
                         continue
                     elif (serialByte == END_MARKER): #If end marker is reached
@@ -89,7 +89,7 @@ def pull():
                     cmdBuffer = packet[index:index+3]
                     index = index + 4
                     if (cmdBuffer == "ROB"): #Check if the received string is "ROB"
-                        print("ROB")
+                        print("ROB") #this is for position of the robot
                         while packet[index+cmdIndex] != VALUE_SEP:
                             cmdIndex = cmdIndex + 1
                         posRobx= int(packet[index:index+cmdIndex])
@@ -116,7 +116,7 @@ def pull():
                         posRobt= int(packet[index:index+cmdIndex])
                         cmdIndex = 0
                         commandReceived = False
-                    elif (cmdBuffer == "OPP"): #Check if the received string is "OPP"
+                    elif (cmdBuffer == "OPP"): #Check if the received string is "OPP" #which indicates tracking the position of opponent
                         while packet[index+cmdIndex] != VALUE_SEP:
                             cmdIndex = cmdIndex + 1
                         posOppx= int(packet[index:index+cmdIndex])
@@ -127,7 +127,7 @@ def pull():
                         posOppy= int(packet[index:index+cmdIndex])
                         cmdIndex = 0
                         commandReceived = False
-                    elif (cmdBuffer == "BAL"): #Check if the received string is "OPP"
+                    elif (cmdBuffer == "BAL"): #Check if the received string is "BAL" #tracking position of the ball
                         while packet[index+cmdIndex] != VALUE_SEP:
                             cmdIndex = cmdIndex + 1
                         posBallx= int(packet[index:index+cmdIndex])
@@ -152,7 +152,7 @@ def isWhiteSpace(character):
         return True
     return False
 
-def push(motorSpeedAbs,inA1,inA2):
+def push(motorSpeedAbs,inA1,inA2): #pushes data TO the arduino from the pi
 
     #<MO0-rpm-in1-in2|MO1-rpm-in1-in2|MO2-rpm-in1-in2|MO3-rpm-in1-in2|>
     packet = "<"
@@ -165,9 +165,9 @@ def push(motorSpeedAbs,inA1,inA2):
     
     ser.write(packet.encode('utf-8'))
 
-speed = np.array([255, 216, 160, 122])
-ina1 = [False, True, False,  True]
-ina2 = [True, False,  True, False]
+speed = np.array([255, 216, 160, 122]) #(TEST VALUES) indicates each of the four motor speeds in order
+ina1 = [False, True, False,  True]  #values of the first gate of each motor driver
+ina2 = [True, False,  True, False]  #values of the second gate of each motor driver
 while True:
     # print(posRobx)
     # print(posRoby)
