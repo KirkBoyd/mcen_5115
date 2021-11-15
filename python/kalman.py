935cc1f74793ee984118230b1e1f6fa49bb1cd15
Kirk Boyd <kirkland.boyd@trincoll.edu>
2021-11-15 12:33:45 -0700

    finish commenting kalman.py for now

--- python/kalman.py
+++ python/kalman.py
@@ -42,65 +42,73 @@ maxRPM = 1000 # Safety value to keep from over running motors
 ## Plotting
 mapScale = .1 #1px = 1cm
 MAP = cv.imread("python\Map.PNG") #Read in picture of map
-width = int(8*305*mapScale)
-height = int(12*305*mapScale)
-dimensions = (width,height)
-MAP = cv.resize(MAP,dimensions, interpolation=cv.INTER_LINEAR)
+width = int(8*305*mapScale) # width of simulation window
+height = int(12*305*mapScale) # height of simulation window
+dimensions = (width,height) # implement width and height of sim window
+MAP = cv.resize(MAP,dimensions, interpolation=cv.INTER_LINEAR) # resize the image to match height and width
+
 ## Classes
-class PosSensor(object):
-    def __init__(self, pos=(0, 0), noise_std=1.):
-        self.noise_std = noise_std
-        self.pos = [pos[0], pos[1]]
+class PosSensor(object): # What does this class do exactly?
+    def __init__(self, pos=(0, 0), noise_std=1.): # constructor for PosSensor class
+        self.noise_std = noise_std # not sure what this means -KB
+        self.pos = [pos[0], pos[1]] # or this
         
-    def read(self):
+    def read(self): # returns position values
         self.pos[0]
         self.pos[1]
         
-        return [self.pos[0] + randn() * self.noise_std,
+        return [self.pos[0] + randn() * self.noise_std, # creates random dummy variables for testing? -KB
                 self.pos[1] + randn() * self.noise_std]
+
 ## Functions
 def motorSpeed(V): #Input vector (vx,vy,theta) [mm/s],[mm/s],[rad/s]
-    global robotMotorSpeed
+    global robotMotorSpeed # is this just the output variable so it is set as global? -KB ##
     T = np.array([[1,-1,-lx-ly],[1,1,lx+ly],[1,1,-lx-ly],[1,-1,lx+ly]])/r #Translation matrix
     motorSpeed = np.matmul(T,V) #Forward Kinematics
     motorSpeed = motorSpeed*maxRPM/max(abs(motorSpeed)) #Normalize rpm
     robotMotorSpeed = motorSpeed
-    motorSpeed = [motorSpeed[0][0],motorSpeed[1][0],motorSpeed[2][0],motorSpeed[3][0]]
+    motorSpeed = [motorSpeed[0][0],motorSpeed[1][0],motorSpeed[2][0],motorSpeed[3][0]] # what does this do? -KB
     motorSpeedNorm = np.interp(motorSpeed,[-1000, 1000],[-255,255]) #Normalize rpm to 255 maximum value
     motorSpeedOut = motorSpeedNorm.astype('int32') #Convert motor rpm to integer
     inA1 = motorSpeedNorm >= 0 #Logic for direction
     inA2 = motorSpeedNorm <= 0 #Logic for direction
     motorSpeedAbs = abs(motorSpeedOut) #Make motor speed a magnitude
     return motorSpeedAbs,inA1,inA2
+
 def world2Robot(cords): #Function to convert world coordinates to robot coordinates
-    dX = cords[0] - posRobx
-    dY = cords[1] -posRoby
-    wTheta = np.arctan2(dY,dX)
+    dX = cords[0] - posRobx # Difference in x position b/w robot and world
+    dY = cords[1] -posRoby # Difference in y position b/w robot and world
+    wTheta = np.arctan2(dY,dX)  # Difference in angle b/w robot and world
     #print(wTheta)
     T = np.array([[-np.cos(posRobt),np.sin(posRobt),0],[np.sin(posRobt),np.cos(posRobt),0],[0,0,1]]) #Transformation matrix
-    delta = np.array([[dX],[dY],[wTheta]]).astype(float)
-    return np.matmul(T,delta)
+    delta = np.array([[dX],[dY],[wTheta]]).astype(float) # Make an array of all of the delta values
+    return np.matmul(T,delta) # what is matmul? -KB
+
 def robot2World(cords): #Takes in robot coordinates (rx, ry, rt) and returns world coordinates (wx,wy,wt)
-    x = posRobx + cords[0]*np.cos(posRobt)-cords[1]*np.sin(posRobt)
-    y = posRoby + cords[0]*np.sin(posRobt)+cords[1]*np.cos(posRobt)
+    x = posRobx + cords[0]*np.cos(posRobt)-cords[1]*np.sin(posRobt) # x vals
+    y = posRoby + cords[0]*np.sin(posRobt)+cords[1]*np.cos(posRobt) # y vals
 
     return [x,y]
+
 def goal2Speed(goal,bias): #Function to get robot velocity based off the curr robot position and goal positions, bias is for turning speed
     robotGoalCords = world2Robot(goal)
-    vx = robotGoalCords[0]
-    vy = robotGoalCords[1]
-    vt = (goal[2] - posRobt)*bias
+    vx = robotGoalCords[0] # what do these do/reference? -KB
+    vy = robotGoalCords[1] # is robotGoalCords[] coordinates or velocities? -KB
+    vt = (goal[2] - posRobt)*bias # what is this too?
     
     return [vx,vy,vt]
+
 def updateVelocity(): #Update the global velocity of the robot for positioning data
     global robotVelocity
     T = np.array([[1,1,1,1],[-1,1,1,-1],[-1/(lx+ly),1/(lx+ly),-1/(lx+ly),1/(lx+ly)]])/r #Translation matrix
     tempVelocity = np.dot(T,robotMotorSpeed) #Inverse Kinematics
+        # dot product of translation matrix and robotMotorSpeed?
     vx = tempVelocity[0]*np.cos(posRobt)-tempVelocity[1]*np.sin(posRobt)
     vy = tempVelocity[0]*np.sin(posRobt)+tempVelocity[1]*np.cos(posRobt)
     vt = tempVelocity[2]
     robotVelocity = [-vx[0],vy[0],vt[0]] #THIS SHIT IS FUCKED (IDK WHY ITS -vx)
-    pass
+    pass # do nothing, but move on
+
 def updatePosRob(): #Updates the position based on the global robot velocity values
     global posRobx
     global posRoby
@@ -112,6 +120,7 @@ def updatePosRob(): #Updates the position based on the global robot velocity val
     posRoby = posRoby + robotVelocity[1]*vScale
     posRobt = posRobt + robotVelocity[2]*vScale
     pass
+
 def robotTriangle(): #Returns the points of a triangle corresponding to the robot's position and orientation.
     L = 250
     p1 = (L*np.sqrt(3)/3,0)
@@ -124,6 +133,7 @@ def robotTriangle(): #Returns the points of a triangle corresponding to the robo
 
     pts = np.array([(int(w1[0]*mapScale),height - int(w1[1]*mapScale)),(int(w2[0]*mapScale),height - int(w2[1]*mapScale)),(int(w3[0]*mapScale),height - int(w3[1]*mapScale))])
     return pts
+
 def updateMap(): #Updates minimap
     map = MAP.copy()
     # Blue for robot
@@ -137,22 +147,31 @@ def updateMap(): #Updates minimap
     cv.drawMarker(map, (int(objective[0]*mapScale),height-int(objective[1]*mapScale)) ,(0,255,0), markerType=cv.MARKER_TILTED_CROSS , markerSize=5, thickness=2, line_type=cv.LINE_AA)
 
     cv.imshow('Map',map)
-    pass
-def scoringPosition():  #Retruns a position and orientation of the robot that is in line with the ball and goal
-    theta = np.arctan2(posTargety - posBally, posTargetx - posBallx)
-    x = posBallx - 150*np.cos(theta)
-    y = posBally - 150*np.sin(theta)
+    pass # do nothing, but move on
+
+def scoringPosition():  #Returns a position and orientation of the robot that is in line with the ball and goal
+    theta = np.arctan2(posTargety - posBally, posTargetx - posBallx) #calculate angle
+    x = posBallx - 150*np.cos(theta) # calculate x dist
+    y = posBally - 150*np.sin(theta) # calculate y dist
     return (x,y,theta)
+
 def positionCheck(position): #Checks if the robot is close enough to its desired position
     distance = 100 #Maximum distance
     angle = np.pi/16 #Maximum angle
     return abs(posRobt-position[2]) < angle and np.sqrt((posRobx - position[0])**2 + (posRoby-position[1])**2) < distance
+        # return boolean which checks;
+            # whether the absolute value of [ if robot position is less than max angle, and, 
+            # when the distance in robot position [the square root of (difference in position_x squared + diff in pos_y squared)]
+            # is less than distance
+        # in other words, is the robot close enough in position and angle to actually start pushing the ball to the goal
+
 def updateBall(): #Updates the position of the ball to infront of the robot
     global posBallx
     global posBally
 
     posBallx = posRobx + 150*np.cos(posRobt)
     posBally = posRoby + 150*np.sin(posRobt)
+
 def defensePosition(): #Returns the point which is closest to the robot on a line from the ball to our goal
     if posBally > posRoby: #Check that the robot is at least goal side
         m = np.array([posBallx-posProtectx,posBally-posProtecty]) #Direction vector of line
@@ -160,19 +179,21 @@ def defensePosition(): #Returns the point which is closest to the robot on a lin
         P = np.array([posRobx,posRoby]) #Robot point
         PA = P-A #From goal to rob
 
-        pose = np.dot(PA,m)/np.linalg.norm(m)**2*m + A
-        point = (pose[0],pose[1],np.arctan2(pose[0],pose[1]))
+        pose = np.dot(PA,m)/np.linalg.norm(m)**2*m + A # not sure what this does -KB
+        point = (pose[0],pose[1],np.arctan2(pose[0],pose[1])) # not sure what this does -KB
     else: point = (posProtectx,posBally,np.pi/2) #go to goal, Can add more functionality
     return point
+
 def checkDefensive(): #Checks if the robot is close enough to the line from the ball to our goal
     m = np.array([posBally-posProtecty,posProtectx-posBallx]) #Direction vector of line
     A = np.array([posProtectx,posProtecty]) #Origin of line
     P = np.array([posRobx,posRoby]) #Robot point
     PA = A-P #From goal to rob
-    d = np.dot(m/np.linalg.norm(m),PA)
+    d = np.dot(m/np.linalg.norm(m),PA) # what does this do? -KB
 
     return d<50
-def push(data): #pushes data TO the arduino from the pi
+
+def push(data): #pushes data TO the arduino from the pi, takes input array of data[] which should be 1x3? -KB
     motorSpeedAbs = data[0]
     inA1 = data[1]
     inA2 = data[2]
@@ -180,19 +201,20 @@ def push(data): #pushes data TO the arduino from the pi
     motor = ""
     outA1 = ""
     outA2 = ""
-    for i in range(4):
+    for i in range(4): # little lost here, let's talk this through and comment it more -KB
         motor = motor + str(motorSpeedAbs[i])
         outA1 = outA1 + str(int(inA1[i]==True))
         outA2 = outA2 + str(int(inA2[i]==True))
-        if i < 3:
+        if i < 3: 
             motor = motor + "-"
             outA1 = outA1 + "-"
             outA2 = outA2 + "-"
         
-        packet = "<MOT|" + motor + "-" + outA1 + "-" + outA2 + ">"
+        packet = "<MOT|" + motor + "-" + outA1 + "-" + outA2 + ">" # construct packet
     
-    ser.write(packet.encode('utf-8'))
-    print(packet.encode('utf-8'))
+    ser.write(packet.encode('utf-8')) # write packet to serial
+    print(packet.encode('utf-8')) # also print packet
+
 def kalman(H,Q,R,dt):
     F = np.array([[1, dt, 0, 0,],   #x    Design State Transformation Matrix
                   [0, 0, 1, dt],    #y
@@ -200,7 +222,7 @@ def kalman(H,Q,R,dt):
                   [0, 0, 0, 1]])    #vy
 
 ## Main Loop
-test = 1
+test = 1 #index variable?
 try:
     while test != 5:
         posOppy = posOppy - 1
@@ -210,6 +232,7 @@ try:
                 objective = scoringPosition() #Try to score
                 if positionCheck(objective): #Robot is touching ball
                     push(motorSpeed((goal2Speed((posTargetx,posTargety,objective[2]),10))))
+                    # ^ send arduino command to ____ drive to target positions? -KB
                     updatePosRob()
                     updateBall()
                 else: #Robot is far from ball
@@ -223,6 +246,7 @@ try:
             objective = scoringPosition()
             if positionCheck(objective): #Robot is touching ball
                 push(motorSpeed((goal2Speed((posTargetx,posTargety,objective[2]),10))))
+                # ^ send arduino command to  ___ also drive to target position? -KB
                 updatePosRob()
                 updateBall()
             else: #Robot is far from ball
