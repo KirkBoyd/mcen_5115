bd87d2b2ae94ffa0585b3071c9e2ced331982b41
ThomasGira <thgi3074@colorado.edu>
2021-12-02 22:48:37 -0700

    Wrote Parse, Pull and Push

--- python/Mecanum Function 3/main.py
+++ python/Mecanum Function 3/main.py
@@ -1,3 +1,4 @@
+#Libraries
 import numpy as np
 import time
 from numpy.core.numeric import ones
@@ -5,6 +6,33 @@ from gpiozero import Button
 from gpiozero import LED
 import serial
 import signal
+
+# GPIO Variables
+team = 'green'
+button = Button(16)
+buttonGRN = Button(5)
+buttonBLU = Button(6)
+ledBLU = LED(23)
+imuCounter = 0
+ledGRN = LED(24)
+radCounter = 0
+ledIMU = LED(25)
+ledRAD = LED(13)
+
+#Serial Communication Initialization and resetting
+ser0 = serial.Serial('/dev/ttyACM0',9600,write_timeout=.5,timeout=.5) #IMU and Motors
+ser1 = serial.Serial('/dev/ttyACM1',9600,write_timeout=.5,timeout=.5) #Radio
+time.sleep(1)
+
+ser0.flush()
+ser0.reset_output_buffer()
+ser0.reset_input_buffer()
+
+ser1.flush()
+ser1.reset_output_buffer()
+ser1.reset_input_buffer()
+connected = True
+
 
 def playSoccer():
     pass
--- python/Mecanum Function 3/world.py
+++ python/Mecanum Function 3/world.py
@@ -1,30 +1,37 @@
 import numpy as np
+import serial
 
-from python.mecanumFunctions import motorSpeed
+from python.mecanumFunctions import isWhiteSpace, motorSpeed, reamonToWorldX
 
 class opponentClass:
-    def __init__(self):
+    def __init__(self,color = "blue"):
         self.x = 0
         self.y = 0
+        self.color = color
 
 class robotClass:
-    def __init__(self):
+    def __init__(self,color = "green"):
         self.x = 0
         self.y = 0
         self.theta = 0
         self.speeds = np.array[0,0,0,0]
-        self.direction = np.array[0,0,0,0]
+        self.directions = np.array[0,0,0,0]
+        self.imuBias = 0
+        self.imuBiasRecieved = False
+        self.color = color
 
 class ballClass:
-    def __init(self):
+    def __init(self,color = "yellow"):
         self.x = 0
         self.y = 0
+        self.color = color
 
 class worldClass():
     def __init__(self):
         self.opponent = opponentClass()
         self.robot = robotClass()
         self.ball = ballClass()
+        self.radioData = np.zeros[6]
 
     def world2robot(self,wx,wy): #Function to convert world to robot coordinates (Complete)
         robotx = self.robot.x + wx*np.cos(self.robot.theta) - wy*np.sin(self.robot.theta)
@@ -40,3 +47,116 @@ class worldClass():
 
     def robot2world(): #Function to convert world coordinates to robot coordinates (Incomplete)
         pass
+
+    def isWhiteSpace(character): #checks if input value from serial is a blank / whitespace character
+        if (character == ' '):
+            return True
+        if (character == '\r'):
+            return True
+        if (character == '\n'):
+            return True
+        if (character == '⸮'):
+            return True
+        return False
+
+    def parseImu(self,inPacket):
+        START_MARKER = '<'  #marks the beginning of a data packet
+        END_MARKER = '>'    #marks the end of a data packet
+        VALUE_SEP = '-'     #?? *Kirk stop being an idiot*
+        receiving = False   #set to true when start marker is received, set to false when end marker is received
+        index = 0
+        lenInPacket = len(inPacket)
+        while index < lenInPacket: #Loops through incoming packet
+            serialByte = inPacket[index] #Grabs a character
+            if isWhiteSpace(serialByte): # Cancel everything if their is a bad character
+                return
+            if serialByte == START_MARKER and not receiving:
+                receiving = True
+                index += 1
+                continue
+            if serialByte == START_MARKER and receiving: #Bad data
+                return
+            if receiving: #Look for packet
+                if serialByte != END_MARKER:
+                    try:
+                        float(inPacket[index])
+                    except ValueError:
+                        return
+                    index += 1
+                if serialByte == END_MARKER:
+                    try:
+                        angle = (float(inPacket[1:index])*2)*(np.pi/180)
+                    except ValueError:
+                        return
+                    if not self.robot.imuBiasRecieved:
+                        self.robot.imuBias = angle
+                        self.robot.imuBiasRecieved = True
+                    self.robot.theta = angle - self.robot.imuBias
+        
+    def parseRadio(self,inPacket):
+        START_MARKER = '<'  #marks the beginning of a data packet
+        END_MARKER = '>'    #marks the end of a data packet
+        VALUE_SEP = '-'     #?? *Kirk stop being an idiot*
+        receiving = False   #set to true when start marker is received, set to false when end marker is received
+        index = 0
+        radioDataIndex = 0
+        valueString = ""
+        lenInPacket = len(inPacket)
+        while index < lenInPacket: #Loops through incoming packet
+            serialByte = inPacket[index] #Grabs a character
+            if isWhiteSpace(serialByte): # Cancel everything if their is a bad character
+                return
+            if serialByte == START_MARKER and not receiving:
+                receiving = True
+                index += 1
+                continue
+            if serialByte == START_MARKER and receiving: #Bad data
+                return
+            if receiving: #Look for packet
+                if serialByte != VALUE_SEP and serialByte != END_MARKER:
+                    try:
+                        float(inPacket[index])
+                        valueString = valueString + inPacket[index]
+                    except ValueError:
+                        print("Error Converting Character To Float in parseRadio")
+                        return
+                elif serialByte == VALUE_SEP:
+                    try:
+                        value = float(valueString)
+                        self.radioData[radioDataIndex] = value
+                        radioDataIndex += 1
+                    except ValueError:
+                        print("Error Converting Character To Float in parseRadio")
+                        return
+                elif serialByte == END_MARKER:
+                    try:
+                        value = float(valueString)
+                        self.radioData[radioDataIndex] = value
+                        radioDataIndex += 1
+                        self.setBallLocations()
+                        return
+                    except ValueError:
+                        print("Error Converting Character To Float in parseRadio")
+                        return
+
+    def setBallLocations(self):
+        #Radio send data in the order (Green,Blue,Yellow)
+        colorList = ["green","blue","yellow"]
+        colorIndex = 0
+        for index in range(3):
+            if self.robot.color == colorList[colorIndex]:
+                worldPositions = self.reamon2world(self.radioData[index*2],self.radioData[index*2+1])
+                self.robot.x = worldPositions[0]
+                self.robot.y = worldPositions[1]
+                return
+            if self.opponent.color == colorList[colorIndex]:
+                worldPositions = self.reamon2world(self.radioData[index*2],self.radioData[index*2+1])
+                self.opponent.x = worldPositions[0]
+                self.opponent.y = worldPositions[1]
+                return
+            if self.ball.color == colorList[colorIndex]:
+                worldPositions = self.reamon2world(self.radioData[index*2],self.radioData[index*2+1])
+                self.ball.x = worldPositions[0]
+                self.ball.y = worldPositions[1]
+                return
+            colorIndex += 1
