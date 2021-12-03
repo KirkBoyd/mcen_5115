70ce2dc044d27f04bba76e0945fba1a761cc39f3
ThomasGira <thgi3074@colorado.edu>
2021-12-02 21:23:52 -0700

    Wrote coordinate functions

--- python/Mecanum Function 3/world.py
+++ python/Mecanum Function 3/world.py
@@ -1,38 +1,38 @@
 import numpy as np
 
-class opponent:
+class opponentClass:
     def __init__(self):
         self.x = 0
         self.y = 0
 
-class robot:
+class robotClass:
     def __init__(self):
         self.x = 0
         self.y = 0
         self.theta = 0
 
-class ball:
+class ballClass:
     def __init(self):
         self.x = 0
         self.y = 0
 
-class world():
+class worldClass():
     def __init__(self):
-        self.opponent = opponent()
-        self.robot = robot()
-        self.ball = ball()
+        self.opponent = opponentClass()
+        self.robot = robotClass()
+        self.ball = ballClass()
 
-    def world2robot(wx,wy):
-        robotx = wy
-        roboty = wy
-        rtheta = 0
-        #rtheta = np.atan2(robotx,roboty) Confirm this before adding
+    def world2robot(self,wx,wy): #Function to convert world to robot coordinates (Complete)
+        robotx = self.robot.x + wx*np.cos(self.robot.theta) - wy*np.sin(self.robot.theta)
+        roboty = self.robot.y + wx*np.sin(self.robot.theta) + wy*np.cos(self.robot.theta)
+
+        rtheta = np.arctan2(roboty,robotx)
         return robotx, roboty, rtheta
 
-    def reamon2world(reamonx,reamony):
+    def reamon2world(reamonx,reamony): #Function to convert camera coordinates to world coordinates (Complete)
         worldx = 500 - reamony
         worldy = 375 - reamonx
         return worldx,worldy
 
-    def robot2world(): #Function to convert world coordinates to robot coordinates
+    def robot2world(): #Function to convert world coordinates to robot coordinates (Incomplete)
         pass
