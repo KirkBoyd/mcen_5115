a117c0ac492f4a905b67637a7c95d9f4e1c4c2ec
ThomasGira <thgi3074@colorado.edu>
2021-12-02 21:10:00 -0700

    Added Classes

---
+++ python/Mecanum Function 3/world.py
@@ -0,0 +1,21 @@
+class opponent:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+class robot:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+
+class ball:
+    def __init(self):
+        self.x = 0
+        self.y = 0
+
+class world():
+    def __init__(self):
+        self.opponent = opponent()
+        self.robot = robot()
+        self.ball = ball()
